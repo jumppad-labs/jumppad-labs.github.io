487e3369f0cacb102bcee82404639b4d852d67ce
Erik Veld <eveld@hashicorp.com>
2023-04-17 08:06:48 +0200

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,31 @@
+import nextMDX from '@next/mdx'
+import { remarkPlugins } from './mdx/remark.mjs'
+import { rehypePlugins } from './mdx/rehype.mjs'
+import { recmaPlugins } from './mdx/recma.mjs'
+
+const isProd = process.env.NODE_ENV === 'production'
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins,
+    rehypePlugins,
+    recmaPlugins,
+    providerImportSource: '@mdx-js/react',
+  },
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  experimental: {
+    scrollRestoration: true,
+  },
+  assetPrefix: isProd ? `/${process.env.GIT_REPOSITORY}/` : '',
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default withMDX(nextConfig)
