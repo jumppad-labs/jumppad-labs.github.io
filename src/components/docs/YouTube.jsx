9c453b028409c20d20ec1d3e87bff74b52575a13
Erik Veld <eveld@hashicorp.com>
2023-05-08 09:44:47 +0200

    Style youtube component and hide some branding

--- src/components/docs/YouTube.jsx
+++ src/components/docs/YouTube.jsx
@@ -1,9 +1,9 @@
 export function YouTube({id='dQw4w9WgXcQ'}) {
-  let url = 'https://www.youtube.com/embed/' + id
+  let url = `https://www.youtube-nocookie.com/embed/${id}?modestbranding=1&rel=0`
 
   return (
     <iframe 
-      class="w-full aspect-video"
+      className="mt-16 rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10 sm:mt-24 w-full aspect-video"
       src={url}
       frameborder="0" 
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
