96464a911f6bcfc51477099ce4b94d73436c7b99
Erik Veld <erik.veld@gmail.com>
2024-07-22 15:11:17 +0200

    fix

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import {  useRouter } from 'next/router'
 import { MDXProvider } from '@mdx-js/react'
 
 // Website
@@ -12,6 +13,8 @@ import '@/styles/tailwind.css'
 import 'focus-visible'
 
 export default function App({ Component, pageProps }) {
+  let router = useRouter()
+
   return (
     <>
       <Head>
@@ -21,6 +24,7 @@ export default function App({ Component, pageProps }) {
           <title>{`${pageProps.title} - Jumppad`}</title>
         )}
         <meta name="description" content={pageProps.description} />
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="6ea0771b-a94e-4958-acdf-b99bca18157d"></script>
       </Head>
       {router.pathname === '/' ? (
           <Index></Index>
